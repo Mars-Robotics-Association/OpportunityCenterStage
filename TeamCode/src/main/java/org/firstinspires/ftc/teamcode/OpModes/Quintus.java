7afa7fd5ded1a50d7f137440336a512afc7c6480
elainaAnderson <elaina.anderson@marsroboticsassociation.org>
2023-12-29 20:09:41 -0500

    better pseudocode

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/Quintus.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/Quintus.java
@@ -27,13 +27,96 @@ public class Quintus
         payload = new Payload(hardwareMap, drive);
     }
 
+
+//---------------------------------------------------------
     //Common autonomous functions
     //TODO:
     //Detect position of team prop (opencv or queen team prop)
+    public int detectPropPos(){
+        /* takes: camera image, if blue/red
+        1. take the camera image,
+        2. determine which of three boxes has highest percent of red/blue pixels in photo,
+        3. output which position the team prop is in
+        */
+        return 2;
+    }
     //Place purple pixel next to team prop
-    //Place yellow pixel in correct position
-    //Park (close → corner; far → middle)
+    public void placePurpPix(int parkPos, int color){
+        /* take: the position found in detectPropPos
+        - have code (case) for positions
+        drive.pos(); //drive to pos
+        2. place pixel :)
+        3. move away from lines (case) (back towards the wall or go through towards mid)
+         */
 
+        //drive.pos();
+        payload.pixelArm.gripperB.open(); //place pixel
+        if (parkPos==0 && color==0) { //0==near wall and red 1==mid and blue
+            //drive.pos(); //back up
+            //drive.pos(); //drive right to corner
+        }
+        else if (parkPos==0 && color==1){
+            //drive.pos(); //back up
+            //drive.pos(); //drive left to corner
+        }
+        else if (parkPos==1 && color==0) {
+            //drive.pos(); //back up
+            //drive.pos(); //drive left
+            //drive.pos(); //drive forward
+            //drive.pos(); //drive right towards corner
+        }
+        else if (parkPos==1 && color==1){
+            //drive.pos(); //back up
+            //drive.pos(); //drive right
+            //drive.pos(); //drive forward
+            //drive.pos(); //drive left through curtain
+        }
+
+
+    }
+    //Place yellow pixel in correct position
+    public void placeYellowPix(int propPos){
+        payload.pixelArm.lift.setHeight(8); //raise lift, TODO find inches value
+        switch(propPos){
+            case 0:
+                //drive to pos 0
+                break;
+            case 1:
+                //drive to pos 1
+                break;
+            case 2:
+                //drive to pos 2
+                break;
+        }
+        payload.pixelArm.gripperA.open(); //open left? gripper (gripper with yellow pixel)
+        //drive.pos();  //drive backwards away from board TODO find pos
+        //lower lift
+        payload.pixelArm.lift.setHeight(0);
+    }
+
+    //Park (close → corner; far → middle)
+    public void autoPark(int parkpos, int color){
+        /* take: if park in corner or mid
+        1. (case) drive to end of auto pos
+         */
+        if (parkpos==0 && color==0){
+            //drive.pos(); //right
+            //drive.pos(); //forward
+        }
+        else if (parkpos==0 && color==1){
+            //drive.pos(); //left
+            //drive.pos(); //forward
+        }
+        else if (parkpos==1 && color==0){
+            //drive.pos(); //left
+            //drive.pos(); //forward
+        }
+        else if (parkpos==1 && color==1) {
+            //drive.pos(); //right
+            //drive.pos(); //forward
+        }
+    }
+//--------------------------------------------------------------------------------------
 
     //Positional util functions
     private Pose2d computeActualPosition(Pose2d readingSample){
